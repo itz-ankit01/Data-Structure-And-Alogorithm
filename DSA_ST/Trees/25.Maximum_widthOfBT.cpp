74ee5ac1c37f09d0fa49505042ce958acc15f0f4
Prince <ankitsharma0004d@gmail.com>
2024-10-12 19:10:32 +0530

    Maximum width of the Binary Tree

---
+++ DSA_ST/Trees/25.Maximum_widthOfBT.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+typedef vector<int> vi;
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+/**
+ * * Width: Number of nodes in a level between any 2 nodes
+ * * width = (last - first + 1)
+ * * 0-based indexing : (2*i+1), (2*i+2)
+ * * 1-based indexing : (2*i), (2*i+1)
+ */
+
+int widthOfBT(Node* root){
+    if(!root) return 0;
+
+    int ans = 0;
+    queue<pair<Node*, int>> q;
+    q.push({root, 0});
+
+    while(!q.empty()){
+        int size = q.size();
+        int mini = q.front().second;
+        int first, last;
+        for(int i=0; i<size; i++){
+            int cur_id = q.front().second;
+            Node* node = q.front().first;
+            q.pop();
+            if(i == 0) first = cur_id;
+            if(i == size - 1) last = cur_id;
+            if(node -> left) q.push({node -> left, cur_id * 2 + 1});
+            if(node -> right) q.push({node -> right, cur_id * 2 + 2});
+        }
+        ans = max(ans, last-first+1);
+    }
+    return ans;
+}
