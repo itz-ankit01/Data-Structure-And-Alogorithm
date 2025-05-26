69fc02e4ab732966e123f8e49e92839bfe8d8bdc
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-05-26 21:30:34 +0530

    Time: 615 ms (5.59%) | Memory: 296.5 MB (20.04%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,36 +1,30 @@
 class Solution {
-    int solve(vector<int>& nums, int ind, int last ,vector<vector<int>>& dp) {
-        if(ind == nums.size()) return 0;
-
-        if(dp[ind][last+1] != -1) return dp[ind][last+1];
-
-        int take = 0;
-        if(last == -1 || nums[ind] > nums[last]) {
-            take = 1 + solve(nums, ind+1, ind, dp);
+    int fun(vector<int>& nums, int ind, int prev, vector<vector<int>>&dp) {
+        if(ind == nums.size()) {
+            return 0;
         }
 
-        int skip = solve(nums, ind+1, last, dp);
+        if(prev != -1 && dp[ind][prev] != -1) return dp[ind][prev];
 
-        return dp[ind][last+1] = max(take, skip);
+        int take = 0;
+        if(prev == -1 || nums[prev] < nums[ind]) {
+            take = 1 + fun(nums, ind+1, ind, dp);
+        }
+
+        int skip = fun(nums, ind+1, prev, dp);
+
+        if(prev != -1) {
+            dp[ind][prev] = max(skip, take);
+        }
+
+        return max(skip, take);
+
     }
+        
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0)); 
-        
-        for(int i = n-1; i>=0; i--) {
-            for(int j = i - 1; j >= -1; j--) {
-                int take = 0;
-                if(j == -1 || nums[i] > nums[j]) {
-                    take = 1 + dp[i+1][i+1];
-                }
-
-                int skip = dp[i+1][j+1];
-
-                 dp[i][j+1] = max(take, skip);
-            }
-        }
-
-        return dp[0][0];
+        vector<vector<int>> dp (n+1, vector<int>(n+1, -1));
+        return fun(nums, 0, -1, dp);
     }
 };
