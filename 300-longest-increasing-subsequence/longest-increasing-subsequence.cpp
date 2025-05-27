14b766d492cc0afcc13b1aa4a74cae27d3d82552
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-05-27 20:36:09 +0530

    Time: 59 ms (63.32%) | Memory: 14.5 MB (46.74%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -24,7 +24,17 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp (n+1, vector<int>(n+1, -1));
-        return fun(nums, 0, -1, dp);
+        int maxi = 1;
+        vector<int> t(n, 1);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(nums[i] > nums[j]) {
+                    t[i] = max(t[i], t[j]+1);
+                    maxi = max(maxi, t[i]);
+                }
+            }
+        }
+        return maxi;
+        
     }
 };
