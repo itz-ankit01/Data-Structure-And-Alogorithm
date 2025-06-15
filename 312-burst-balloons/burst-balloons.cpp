3a3b5ea0852f25c8ca98168329bbd6f889faccb7
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-06-15 17:23:57 +0530

    Time: 96 ms (73.33%) | Memory: 14.3 MB (7.06%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -15,7 +15,19 @@ public:
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return fun(1, n-2, nums, dp);
+        vector<vector<int>> dp(n+10, vector<int>(n+10, 0));
+
+        for(int i=n-2; i>=1; i--) {
+            for(int j=1; j<=n-2; j++) {
+                if(i > j) continue;
+                int mx = -1e9;
+                for(int ind = i; ind <= j; ind++) {
+                    int cost = nums[i-1]*nums[ind]*nums[j+1] + dp[i][ind-1] + dp[ind+1][j];
+                    mx = max(mx, cost);
+                }
+                dp[i][j] = mx;
+            }
+        }
+        return dp[1][n-2];
     }
 };
