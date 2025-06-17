e2197d3f1f23bbdeb2b608f18d228d96faf7dfb8
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-06-17 17:31:03 +0530

    Time: 1263 ms (55.53%) | Memory: 9.4 MB (65.04%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    bool isPalindrome(int i, int j, string & tmp) {
+        while(i < j) {
+            if(tmp[i] != tmp[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    int fun(int i, string &s, vector<int> &dp) {
+        if(i == s.size()) return 0;
+        if(dp[i] != -1) return dp[i];
+      
+        int mini = 1e9;
+        for(int j = i; j < s.size(); j++) {
+           
+             if(isPalindrome(i, j, s)) {
+                int cost = 1 + fun(j+1, s, dp);
+                mini = min(mini, cost);
+             }
+            
+        }
+        return dp[i] = mini;
+    }
+public:
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> dp(n+1, -1);
+        return fun(0, s, dp)-1;
+    }
+};
