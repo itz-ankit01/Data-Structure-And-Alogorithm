6a2c51a2255c8d6763fc78c0745f366e4b0be2f1
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-06-26 21:34:09 +0530

    Time: 16 ms (20.83%) | Memory: 36.1 MB (44.49%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,65 @@
+class Solution {
+        void fun(int row, int col, vector<vector<int>> &vis, vector<vector<int>>&grid, int delRow[], int delCol[]) {
+        vis[row][col] = 1;
+        queue<pair<int,int>> q;
+        q.push({row, col});
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        while(!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+
+            q.pop();
+
+            for(int i=0; i<4; i++) {
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] ==  1) {
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
+    }
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        for(int j = 0; j < m; j++) {
+            if(grid[0][j] == 1) {
+                fun(0, j, vis, grid, delRow, delCol);
+            }
+            if(grid[n-1][j] == 1) {
+                fun(n-1, j, vis, grid, delRow, delCol);
+            }
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(grid[i][0] == 1) {
+                fun(i, 0, vis, grid, delRow, delCol);
+            }
+            if(grid[i][m-1] == 1) {
+                fun(i, m-1, vis, grid, delRow, delCol);
+            }
+        }
+        int ways = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(vis[i][j] == 0 && grid[i][j] == 1) {
+                    ways++;
+                }
+            }
+        }
+
+        return ways;
+    }
+};
