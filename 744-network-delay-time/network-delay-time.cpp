22ef4f955df3a90d49c2f57959a56449d53f691c
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-07-09 15:40:38 +0530

    Time: 86 ms (53.35%) | Memory: 47.6 MB (43.29%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<pair<int,int>> adj[n+1];
+
+        for(auto it: times) {
+            adj[it[0]].push_back({it[1], it[2]});
+        }
+
+        vector<int> dist(n+1, 1e9);
+        dist[k] = 0;
+
+        priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
+
+        pq.push({0, k});
+
+        while(!pq.empty()) {
+            int dis = pq.top().first;
+            int node = pq.top().second;
+
+            pq.pop();
+
+            for(auto it: adj[node] ) {
+                int adjNode = it.first;
+                int edgwt = it.second;
+
+                if(edgwt + dis < dist[adjNode]) {
+                    dist[adjNode] = edgwt + dis;
+                    pq.push({edgwt + dis, adjNode});
+                }
+            }
+        }
+
+        for(auto it: dist) cout << it << endl;
+
+        int ans = 0;
+
+        for(int i = 1; i <= n; i++) {
+            ans = max(ans, dist[i]);
+        }
+        return ans == 1e9 ? -1 : ans;
+    }
+};
