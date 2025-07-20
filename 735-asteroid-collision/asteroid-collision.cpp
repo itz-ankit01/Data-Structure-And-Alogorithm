ed330977ecadd36030112cd26e994880fc3ebe18
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-07-20 10:33:07 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (96.81%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st;
+        vector<int> st;
         for(auto it: asteroids) {
-            while(!st.empty() && it < 0 && abs(it) > st.top() && st.top() > 0) {
-                st.pop();
+            if(it > 0) st.push_back(it);
+            else {
+                while(!st.empty() && st.back() > 0 && st.back() < abs(it)) {
+                    st.pop_back();
+                }
+
+                if(!st.empty() && st.back() == abs(it)) {
+                    st.pop_back();
+                }
+                else if(st.empty() || st.back() < 0) {
+                    st.push_back(it);
+                }
             }
-            if(!st.empty() && st.top() > abs(it) && it < 0) continue;
-            if(st.size() == 0) st.push(it);
-            else if(st.top() == abs(it) && it < 0) {
-                st.pop();
-            }
-            else st.push(it);
         }
-        vector<int> ans;
-        while(!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        
+        return st;
     }
 };
