15b0b42c837d9bd7932588bec292f049b39bed4e
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-07-20 10:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (62.53%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<pair<int, int>> st;
-        int n = asteroids.size();
-
-        st.push({asteroids[n - 1], asteroids[n - 1] / abs(asteroids[n - 1])});
-
-        for (int i = n - 2; i >= 0; i--) {
-            int z = asteroids[i];
-            int dir = z / abs(z);  // direction: +1 or -1
-
-            while (!st.empty() && st.top().second == -1 && dir == 1 && z > abs(st.top().first)) {
+        stack<int> st;
+        for(auto it: asteroids) {
+            while(!st.empty() && it < 0 && abs(it) > st.top() && st.top() > 0) {
                 st.pop();
             }
-
-            if (!st.empty() && st.top().second == -1 && dir == 1) {
-                // Possible collision
-                int topVal = abs(st.top().first);
-                if (topVal == z) {
-                    st.pop(); // both destroy
-                } else if (topVal > z) {
-                    // top survives, do nothing
-                } else {
-                    // current survives
-                    st.pop(); // remove top
-                    st.push({z, dir});
-                }
-            } else {
-                // No collision, safe to push
-                st.push({z, dir});
+            if(!st.empty() && st.top() > abs(it) && it < 0) continue;
+            if(st.size() == 0) st.push(it);
+            else if(st.top() == abs(it) && it < 0) {
+                st.pop();
             }
+            else st.push(it);
         }
-
         vector<int> ans;
-        while (!st.empty()) {
-            ans.push_back(st.top().first);
+        while(!st.empty()) {
+            ans.push_back(st.top());
             st.pop();
         }
-        
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
