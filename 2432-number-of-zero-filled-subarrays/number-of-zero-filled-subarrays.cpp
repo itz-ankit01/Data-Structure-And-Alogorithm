be296c54d15ac2d24e2917d5d72997894ad1931a
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-08-19 22:00:11 +0530

    Time: 6 ms (13.82%) | Memory: 111.2 MB (74.50%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        int i = 0, j = 0;
-        int n = nums.size();
         long long ans = 0;
-
-        while(j < n) {
-            if(nums[j] == 0) j++;
-            else {
-                long long k = j - i;
-                ans += ((k * (k+1))/2);
-                j++;
-                i = j;
+        int cnt = 0;
+        for(int num : nums) {
+            if(num == 0) {
+                cnt++;
+                ans += cnt;
             }
-        }
-        if(i!=j) {
-            long long k = j - i;
-            ans += ((k * (k+1))/2);
+            else cnt = 0;
         }
         return ans;
     }
