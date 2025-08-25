a65957c41572eb23c6777123d85c57438f084bc6
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-08-25 12:16:43 +0530

    Time: 2 ms (43.29%) | Memory: 19.5 MB (38.43%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -17,21 +17,37 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, 0));
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        // dp[0][0] = 0;
+        // dp[0][1] = -prices[0];
 
-        for(int i=1; i<n; i++) {
-            for(int buy=0; buy<2; buy++) {
-                if(buy == 1) {
-                    dp[i][buy] = max(-prices[i]+dp[i-1][0] , dp[i-1][1]);
+        // for(int i=1; i<n; i++) {
+        //     for(int buy=0; buy<2; buy++) {
+        //         if(buy == 1) {
+        //             dp[i][buy] = max(-prices[i]+dp[i-1][0] , dp[i-1][1]);
+        //         }
+        //         else {
+        //             dp[i][buy] = max(prices[i] + dp[i-1][1], dp[i-1][0]);
+        //         }
+        //     }
+        // }
+
+        // return dp[n-1][0];
+
+        dp[n][0] = dp[n][0] = 0;
+
+        for(int i=n-1 ; i>= 0; i--) {
+            for(int buy = 0; buy < 2; buy++) {
+                if(buy) {
+                    dp[i][buy] = max(-prices[i] + dp[i+1][0] , dp[i+1][1]);
                 }
                 else {
-                    dp[i][buy] = max(prices[i] + dp[i-1][1], dp[i-1][0]);
+                    dp[i][buy] = max(prices[i] + dp[i+1][1], dp[i+1][0]);
                 }
             }
         }
 
-        return dp[n-1][0];
+        return dp[0][1];
+
     }
 };
