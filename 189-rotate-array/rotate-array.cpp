bc757e6628348323c0a05c3507dcf7337bbcc1bd
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-09-18 14:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (38.24%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,14 +1,15 @@
 class Solution {
+    void reverse(vector<int>&nums, int s, int e) {
+        while(s < e ) {
+            swap(nums[s++], nums[e--]);
+        }
+    }
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> v(n);
-        k = k%n;
-        for(int i = 0; i < n; i++) {
-            v[i] = nums[(i+n-k)%n];
-        }
-        for(int i = 0; i < n; i++) {
-            nums[i] = v[i];
-        }
+        k = k % n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
     }
 };
