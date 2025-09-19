6851da671e2b9d683a69d4689c7351f41f357013
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-09-19 09:41:07 +0530

    Time: 7 ms (45.49%) | Memory: 16.2 MB (6.01%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -6,14 +6,34 @@ public:
 
         vector<int> ans;
 
+        map<int, int> mp;
         for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(nums[i] + nums[j] == target) {
+            mp[nums[i]]++;
+        }
+        for(int i=0; i<n; i++) {
+            int k = target-nums[i];
+            if(mp.find(k) != mp.end()) {
+                if(k!=nums[i]) {
                     ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
+                    for(int j = i+1; j<n; j++) {
+                        if(nums[j] == k){
+                            ans.push_back(j);
+                            return ans;
+                        }
+                    }
+                } else {
+                    if(mp[k] > 1) {
+                        ans.push_back(i);
+                        for(int j = i+1; j<n; j++) {
+                            if(nums[j] == k){
+                                ans.push_back(j);
+                                return ans;
+                            }
+                        }
+                    }
                 }
             }
+
         }
         return {};
     }
