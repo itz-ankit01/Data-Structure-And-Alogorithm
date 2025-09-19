ca2ef80bb223282a90bc6c0032e4eaeaef9e26cf
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-09-19 10:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.93%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0, j = 0;
+        int n = nums.size();
+
+        while(j < n) {
+            if(nums[j] == 0) {
+                swap(nums[i], nums[j]);
+                i++; j++;
+            }
+            else j++;
+        }
+
+        j = i;
+        while(j < n) {
+            if(nums[j] == 1) {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
+            else j++;
+        }
+    }
+};
