0d4b5d9796f5b0e56ce380ec1c3870977bf47451
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-09-19 09:59:28 +0530

    Time: 2 ms (75.32%) | Memory: 14.6 MB (58.31%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,12 +2,23 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        map<int,int> mp;
+        vector<pair<int, int>> v(n);
+
         for(int i=0; i<n; i++) {
-            if(mp.find(target - nums[i]) != mp.end()) {
-                return {i, mp[target - nums[i]]};
+            v[i] = {nums[i], i};
+        }
+
+        sort(v.begin(), v.end());
+
+        int l = 0, r = n-1;
+
+        while(l < r) {
+            int sum = v[l].first + v[r].first;
+            if(sum == target) {
+                return {v[l].second, v[r].second};
             }
-            else mp[nums[i]] = i;
+            else if(sum < target) l++;
+            else r--;
         }
 
         return {};
