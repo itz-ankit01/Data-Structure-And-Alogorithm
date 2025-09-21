eebb9c87b198f8d33135374f17b357175308e95d
Naruto Codes <116054971+itz-ankit01@users.noreply.github.com>
2025-09-21 11:14:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (95.20%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp;
         int n = nums.size();
-        for(auto it: nums) {
-            mp[it]++;
-        }
-
+       
         vector<int> ans;
-        for(auto it: mp) {
-            if(it.second > n/3) ans.push_back(it.first);
+        int cnt1 = 0, cnt2 = 0, el1 = INT_MIN, el2 = INT_MIN;
+
+        for(int i = 0; i < n; i++) {
+            if(cnt1 == 0 && nums[i] != el2) {
+                cnt1 = 1;
+                el1 = nums[i];
+            }
+            else if(cnt2 == 0 && nums[i] != el1) {
+                cnt2 = 1;
+                el2 = nums[i];
+            }
+            else if(el1 == nums[i]) cnt1++;
+            else if(el2 == nums[i]) cnt2++;
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1 = 0, cnt2 = 0;
+        for(int i = 0; i < n; i++) {
+            if(el1 == nums[i]) cnt1++;
+            else if(el2 == nums[i]) cnt2++;
         }
 
+        if(cnt1 > n/3) ans.push_back(el1);
+        if(cnt2 > n/3) ans.push_back(el2);
         return ans;
     }
 };
